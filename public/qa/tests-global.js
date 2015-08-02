cb193bf42b932132acf481a546838b7a7dc5b783
Jamel Hamani <Owner@Jamels-MacBook-Air.local>
2015-08-02 12:15:21 -0400

    too much to say

--- public/qa/tests-global.js
+++ public/qa/tests-global.js
@@ -2,7 +2,7 @@ suite('Global Tests', function(){
 
 	test('page has a valid title',
 		function(){
-			assert(document.title && document.title.match(/\S/))&&
+			assert(document.title && document.title.match(/\S/)&&
 			document.title.toUpperCase()!== 'TODO');
 
 
